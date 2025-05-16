ee633d0b2d7e5ef1ce3de6a11e0d1a57bf5bef48
KYuSamsonov <KYuSamsonov@tum.beeline.ru>
2025-05-16 08:19:45 +0300

    SFDM-2575 Разработать метод GET api/v1/documentation/{entity-type}/{id}

--- src/main/java/ru/beeline/documentservice/repository/DocumentationTypeRepository.java
+++ src/main/java/ru/beeline/documentservice/repository/DocumentationTypeRepository.java
@@ -3,8 +3,11 @@ package ru.beeline.documentservice.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.beeline.documentservice.domain.DocumentationType;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface DocumentationTypeRepository extends JpaRepository<DocumentationType, Integer> {
     Optional<DocumentationType> findByFolder(String folder);
+
+    List<DocumentationType> findByTargetEntityType(String targetEntityType);
 }
