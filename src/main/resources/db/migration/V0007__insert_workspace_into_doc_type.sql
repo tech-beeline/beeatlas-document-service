4bfb25eaf6233f201942bef848003d0c4dad2375
salikhanov <SAlikhanov@oms.beeline.ru>
2025-09-29 15:47:34 +0600

    SFDM-2908 Добавить новый тип документации миграцией

--- src/main/resources/db/migration/V0007__insert_workspace_into_doc_type.sql
+++ src/main/resources/db/migration/V0007__insert_workspace_into_doc_type.sql
@@ -1,8 +1,8 @@
 INSERT INTO documents.documentation_type (id, name, target_entity_type, ttl, folder, doc_type)
 SELECT COALESCE(MAX(id), 0) + 1,
-       'Описание архитектуры',
-       'workspace',
+       'Архитектурное описание паттерна',
+       'pattern-arch',
        9999,
-       'workspace',
+       'pattern_arch_description',
        'dsl'
 FROM documents.documentation_type;
